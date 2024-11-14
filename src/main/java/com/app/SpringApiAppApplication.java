1809235a6765b29dd9e0112f712a313c758ac1d0
goodBoy <abcd1997@naver.com>
2024-11-14 22:59:27 +0900

    git test

--- src/main/java/com/app/SpringApiAppApplication.java
+++ src/main/java/com/app/SpringApiAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApiAppApplication {
 
 	public static void main(String[] args) {
+		//git test
 		SpringApplication.run(SpringApiAppApplication.class, args);
 	}
 
